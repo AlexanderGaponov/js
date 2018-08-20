dd3037b948b263162b432dc65b3623ff862449ca
Alexander <gaponov@sibmail.com>
2018-08-20 18:14:45 +0700

    Что смог, сделал

---
+++ lesson-4/js/lesson-04.js
@@ -0,0 +1,98 @@
+let money, 
+		name, 
+    time,
+    price; 
+
+function start(){
+	 money = prompt("Ваш бюджет?", '');
+
+	 while(isNaN(money) || money == '' || money == null){
+	 	money = prompt("Ваш бюджет?");
+	 }
+	 name = prompt("Название вашего магазина?", '').toUpperCase();
+   time = 19;
+}
+
+//start();
+
+let mainList = {
+		budget: money,
+		shopName: name,
+		shopGoods: [],
+		employers: {},
+		open: false,
+		discount: false,
+		shopItems: [],
+		chooseGoods: function chooseGoods() { 
+for (let i = 0; i < 5; i++) {
+	let a = prompt("Какой тип товара будем продавать?", '');
+				if( (typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50 ) {
+
+						console.log('Всё верно!');
+						mainList.shopGoods[i] = a;
+
+				} else {
+					i = i - 1;
+				}
+			}
+		},
+		workTime: function workTime(time) {
+				if (time < 0) {
+					console.log('Такого не может быть');
+				} else if (time > 8 && time < 20) {
+					console.log('Время работать!');
+					mainList.open = true;
+					} else if(time < 24) {
+					console.log('Уже слишком поздно!');
+						} else {
+							console.log('В сутках только 24 часа!');
+						};
+			},
+			dayBudget: function dayBudget () {
+				alert("Ежедневный бюджет" + mainList.budget / 30);
+			}, 
+			makeDiscount: function makeDiscount () {
+
+				price = prompt("Какая стоимость товара?", '');
+					
+					if (mainList.discount == true) {
+						console.log ((price * 80)/100);
+					} else {
+						console.log (price);
+					}
+				},
+			hireEmployers: function hireEmployers () {
+				for(let i = 0; i < 4; i++){
+					let name = prompt("Имя сотрудника:", '');
+					mainList.employers[i] = name;
+				}
+			},
+			chooseShopItems: function chooseShopItems (){
+				for(let i = 0; i < 5; i++) {
+				
+				let items = prompt("Перечислите через запятую ваши товары","");
+					
+					if ((typeof(items)) === 'string' && (typeof(items)) != null && a != '') {
+						mainList.shopItems[i] = items;
+						mainList.shopItems = items.split(",");
+						mainList.shopItems.push(prompt("Подождите, ещё ", ""));
+						mainList.shopItems.sort();
+					} else {
+						i = i - 1;
+					}
+					mainList.shopItems.forEach(function(items, i,mainList.shopItems){
+						console.log('У нас вы можете купить' + items)
+					})
+				}
+			}
+ }; 
+
+ for (let key in mainList) {
+ 		console.log('Наш магазин включает в себя:' + key)
+ }
+
+ mainList.chooseShopItems();
+
+console.log(mainList)
+
+
---
+++ lesson-5/Apple/js/script.js
@@ -0,0 +1,18 @@
+let menu = document.querySelectorAll('.menu'),
+		menu_item = document.querySelectorAll('.menu-item'),
+		column = document.querySelectorAll('.column'),
+		body = document.getElementsByTagName('body'),
+		adv = document.querySelectorAll('.adv'); 
+
+
+
+
+//document.column[2].removeChild(adv);
+
+//document.menu.insertBefore(menu_item[2], menu_item[1]);
+
+//let li = document.createElement('li');
+//li.classList.add("menu-item");
+//document.ul.appendChild(li);
+
+//body.style.backgroundImage = 'url:(../img/apple_true.jpg)';//Почему не работает? 
