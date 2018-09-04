fd8a5a2b9819585f6d3cb361950d73df6efe1e6a
Alexander <gaponov@sibmail.com>
2018-09-04 10:46:43 +0700

    15 ое задание

--- lesson-15/test.js
+++ lesson-15/test.js
@@ -3,24 +3,24 @@ let assert = require('chai').assert;
 
 describe("Тип данных", function() {
     it("Функция sum(2,2) возвращает тип данных true!", function() {
-        assert.isFalse(sum(2, 2));
+        assert.isFalse(sum(2, 2))
     })
 })
 
 describe("Соответствие", function() {
     it("Num равное 5! ", function() {
-        assert.equal(num, 5);
+        assert.equal(num, 5)
     })
 })
 
 describe("Each", function() {
     it("Функция each возвращает тип данных объект! ", function() {
-        assert.equal(typeof(each(arr, myFunc)), 'object');
+        assert.equal(typeof(each(arr, myFunc)), 'object')
     })
     it("Соответствует ожидаемому результату! ", function() {
-        assert.equal(each(arr, myFunc).join(), '8,7,6,5,4');
+        assert.equal(each(arr, myFunc).join(), '8,7,6,5,4')
     })
     it("Длина строки соответсвует ожидаемому результату", function() {
-        assert.lengthOf(each(arr, myFunc), 5);
+        assert.lengthOf(each(arr, myFunc), 5)
     })
 })
